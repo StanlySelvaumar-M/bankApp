80c14f8eaad636d921f9bcd7d58f52cee5ede546
Stanly <zohanstanly1990@gmail.com>
2022-06-26 12:40:21 +0530

    reactive form-register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -15,9 +15,10 @@ export class RegisterComponent implements OnInit {
 
   // form group
   registerForm = this.fb.group({
-    acno:'',
-    pwsd:'',
-    uname:''
+    uname:['',[Validators.required,Validators.pattern('[a-zA-Z]*')]],
+    acno:[''],
+    pwsd:['']
+    
   })
 
   constructor(private ds:DataService,private router:Router,private fb:FormBuilder) { }
@@ -25,18 +26,35 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
    register(){
-    var uname=this.uname
-    var acno=this.acno
-    var pswd=this.pswd
+    console.log(this.registerForm.value.uname);
+    
+    var uname=this.registerForm.value.uname
+    var acno=this.registerForm.value.acno
+    var pswd=this.registerForm.value.pwsd
 
-    const result=this.ds.register(uname,acno,pswd)
-
-    if(result){
-      alert ("successfully registered")
-     this.router.navigateByUrl("")
+    if(this.registerForm.get('uname')?.errors){
+      alert("Invalid Username")
     }
+    
+
+
+
+    if(this.registerForm.valid){
+      const result=this.ds.register(uname,acno,pswd)
+
+      if(result){
+        alert ("successfully registered")
+       this.router.navigateByUrl("")
+      }
+      else{
+        alert("already Existing Customer....Please Login!!!!")
+      }
+    }
+
     else{
-      alert("already Existing Customer....Please Login!!!!")
+      alert("Invalid Form")
     }
+
+    
    }
 }
